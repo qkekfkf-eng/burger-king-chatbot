780ed7ac634e81a7e2505ed8499642013fb034a4
qkekfkf <qkekfkf@gmail.com>
2026-05-18 23:06:38 -0400

    first deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+js id="j4n8t2"
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("BK Proxy funcionando");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor activo");
+});
